320bcdf767244e83926195464848cc1d6753deab
towbee98 <tobiemma200@gmail.com>
2021-04-01 12:35:52 +0100

    feat:add the delete and update route

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
-
-app.use(express.json());
-
 const PORT = 3001;
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const jobs = JSON.parse(fs.readFileSync("jobs.json"));
 
-// This get all jobs from the server
-app.get("/api/v1/jobs", (req, res) => {
+//ROUTE HANDLERS
+const getAllJobs = (req, res) => {
   res.status(200).json({
     status: "success",
+    requestMadeAt: req.requestTime,
     result: jobs.length,
     data: {
       jobs,
     },
   });
-});
+};
 
-//this gets a particular job from the server
-app.get("/api/v1/jobs/:id", (req, res) => {
+const getJob = (req, res) => {
   const id = req.params.id * 1;
   const job = jobs.find((el) => el._id === id);
   if (!job) {
@@ -31,29 +38,29 @@ app.get("/api/v1/jobs/:id", (req, res) => {
   }
   res.status(200).json({
     status: "success",
+    requestMadeAt: req.requestTime,
     data: {
       job,
     },
   });
-});
+};
 
-//Create a new job and save on the server
-app.post("/api/v1/jobs", (req, res) => {
+const postAJob = (req, res) => {
   const newId = jobs.length + 1;
   const newJob = Object.assign({ _id: newId }, req.body);
   jobs.push(newJob);
   fs.writeFile("jobs.json", JSON.stringify(jobs), (err) => {
     res.status(201).json({
       status: "success",
+      requestMadeAt: req.requestTime,
       data: {
         newJob,
       },
     });
   });
-});
+};
 
-// Update a  particular job
-app.patch("/api/v1/jobs/:id", (req, res) => {
+const updateJob = (req, res) => {
   const id = req.params.id * 1;
   if (id > jobs.length) {
     return res.status(404).json({
@@ -66,13 +73,39 @@ app.patch("/api/v1/jobs/:id", (req, res) => {
   fs.writeFile("jobs.json", JSON.stringify(jobs), (err) => {
     res.status(200).json({
       status: "success",
+      requestMadeAt: req.requestTime,
       data: {
         updatedJob,
       },
     });
   });
-});
+};
 
+const deleteJob = (req, res) => {
+  const id = req.params.id * 1;
+  const job = jobs.find((el) => el._id === id);
+  if (job) {
+    jobs.splice(job._id - 1, 1);
+    fs.writeFile("jobs.json", JSON.stringify(jobs), (err) => {
+      return res.status(204).json({
+        status: "success",
+        requestMadeAt: req.requestTime,
+        data: null,
+      });
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+};
+
+//ROUTES
+app.route("/api/v1/jobs").get(getAllJobs).post(postAJob);
+app.route("/api/v1/jobs/:id").get(getJob).patch(updateJob).delete(deleteJob);
+
+//START SERVER
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
 });
