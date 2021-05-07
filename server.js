fd1506a1cdc32116bd8210674b9e3d292c1870c0
towbee98 <tobiemma200@gmail.com>
2021-05-07 13:41:29 +0100

    refactor:add the uncaught exception handler  and the rejection handler

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Uncaught Exception , Shutting Down... ");
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
-const DB = process.env.DATABASE.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD
-);
-//const DB = process.env.DATABASE_LOCAL;
+// const DB = process.env.DATABASE.replace(
+//   "<password>",
+//   process.env.DATABASE_PASSWORD
+// );
+const DB = process.env.DATABASE_LOCAL;
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -16,15 +23,24 @@ mongoose
   })
   .then(() => {
     console.log("Database Connected Successfully!!");
-  })
-  .catch((err) => {
-    console.log(err);
   });
+// .catch((err) => {
+//   console.log(err);
+// });
 
 const app = require("./app");
 const PORT = process.env.PORT || 3001;
 
 //START SERVER
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
 });
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled Rejection , Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
