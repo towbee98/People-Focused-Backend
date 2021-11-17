03be465599390dbc37c506bf1d0b6d9d1a2bdb3d
Oladele Tobiloba <tobiemma200@gmail.com>
2021-11-17 05:38:23 +0100

    feat:work on the forget password feature

--- utils/email.js
+++ utils/email.js
@@ -1,29 +1,52 @@
 const nodemailer = require("nodemailer");
-//const dotenv = require("dotenv");
+const pug = require("pug");
+const {convert}=require("html-to-text");
 
-//dotenv.config({ path: "./../config.env" });
+module.exports= class Email{
+  constructor(user,url){
+    this.to= user.email;
+    this.firstName=user.firstName;
+    this.url=url;
+    this.from = `Oladele Tobiloba <${process.env.Email_from}>`;
+  }
 
-const sendEmail = async (options) => {
-  //1.)Create a Transporter
-  const transporter = await nodemailer.createTransport({
-    host: process.env.Email_Host,
-    port: process.env.Email_Port,
-    auth: {
-      user: process.env.Email_Username,
-      pass: process.env.Email_Password,
-    },
-  });
-
-  //2.)Define the email options
-  const mailOptions = {
-    from: "Oladele Tobiloba <tobiemma200@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
+   newTransport(){
+    if(process.env.NODE_ENV==="production"){
+      //Gmail
+    return 1
+    }
+    return  nodemailer.createTransport({
+      host: process.env.Email_Host,
+      port: process.env.Email_Port,
+      auth: {
+        user: process.env.Email_Username,
+        pass: process.env.Email_Password,
+      },
+    });
+  }
+   //Send the actual email
+  async send(template,subject){
+   //1. Render html based on a pug template
+   const html =pug.render(`./../views/emails/${template}.pug`,{
+     firstName:this.firstName,
+     url:this.url,
+     subject,
+   })
+   //2. Define email options
+   const mailOptions = {
+    from: this.from,
+    to: this.to,
+    subject,
+    html,
+    text:convert(html,{wordwrap:130}),
   };
+   //3. Define a transport and send email
+   await this.newTransport().sendMail(mailOptions)
+  }
 
-  //3.) Actually send the email
-  await transporter.sendMail(mailOptions);
-};
+  async sendWelcome(){
+    await this.send("Welcome","Welcome to peopleFocusedNg.com")
+  }
+}
 
-module.exports = sendEmail;
+
